54e924f46d651671eb50abcad058ff6d0e931db5
Miroslav <maksunov.mir96@gmail.com>
2020-02-20 00:03:13 +0500

    4. Квадратное уравнение

--- 4. Квадратное уравнение/main.go
+++ 4. Квадратное уравнение/main.go
@@ -6,28 +6,27 @@ import (
 )
 
 func main() {
-	var a, b, c float64
+	var a, b, c, dis float64
 
 	fmt.Scan(&a, &b, &c)
+	dis = discriminant(a, b, c)
 
-	if discriminant(a, b, c) < 0 {
+	if dis < 0 {
 		println("Данное уравнение не имеет корней")
-	} else {
-		x1, x2 := roots(a, b, discriminant(a, b, c))
-		println("Корни", x1, x2)
+		return
 	}
+
+	x1, x2 := roots(a, b, dis)
+	println("Корни", x1, x2)
 }
 
-func discriminant(a float64, b float64, c float64) float64 {
-
-	dis := (b * b) - (4 * a * c)
-	return dis
+func discriminant(a, b, c float64) float64 {
+	return (b * b) - (4 * a * c)
 }
 
-func roots(a float64, b float64, discriminant float64) (float64, float64) {
+func roots(a, b, dis float64) (float64, float64) {
+	x1 := (-b + math.Pow(dis, 0.5)) / (2 * a)
+	x2 := (-b - math.Pow(dis, 0.5)) / (2 * a)
 
-	x1 := (-b + math.Pow(discriminant, 0.5)) / (2 * a)
-	x2 := (-b - math.Pow(discriminant, 0.5)) / (2 * a)
 	return x1, x2
-
 }
